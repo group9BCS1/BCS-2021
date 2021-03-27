02ad337563b4c88d16f56c44d3384ef23e9a1e0a
grouo9BCS1 <wesongabob@yahoo.com>
2021-03-27 16:46:34 +0300

    Assignment 2

--- src/chapter2/exercise7.py
+++ src/chapter2/exercise7.py
@@ -1,25 +1,26 @@
 #This program lets a user input an amount of money that they need to make change for, and dispenses the correct amount of change
 
 #Prompt the user to enter the change
-amt = float(input('Enter an amount to make a change for: '))
+change = float(input('Enter an amount to make a change for: '))
 print('Your change is... ')
 
-print(int(amt//20), 'twenties')
-amt = amt % 20
-print(int(amt//10), 'tens')
-amt = amt % 10
-print(int(amt//5), 'fives')
-amt = amt % 5
-print(int(amt//1), 'ones')
-amt = amt % 1
-print(int(amt//0.25), 'quarters')
-amt = amt % 0.25
-print(int(amt//0.1), 'dimes')
-amt = amt % 0.1
-print(int(amt//0.05), 'nickels')
-amt = amt % 0.05
-print(round(amt//0.01), 'pennies')
-amt = amt % 0.01
+print(int(change//20), 'twenties')
+change = change % 20
+print(int(change//10), 'tens')
+change = change % 10
+print(int(change//5), 'fives')
+change = change % 5
+print(int(change//1), 'ones')
+change = change % 1
+print(int(change//0.25), 'quarters')
+change = change % 0.25
+print(int(change//0.1), 'dimes')
+change = change % 0.1
+print(int(change//0.05), 'nickels')
+change = change % 0.05
+print(int(change//0.01), 'pennies')
+
+
 
 
 
