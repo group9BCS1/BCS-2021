51f1ec5abb56ec7348ae59f4ce2565c272196bf0
grouo9BCS1 <wesongabob@yahoo.com>
2021-04-18 15:53:06 +0300

    Assignment4

--- src/chapter4/exercise2.py
+++ src/chapter4/exercise2.py
@@ -1,19 +1,26 @@
 #This program computes compound interest
 
 #Prompt the user to input the inital investment
-C = int(input('Enter the initial amount of an investment(C): '))
+C = input('Enter the initial amount of an investment(C): ')
 
 #Prompt the user to input the yearly rate of interest
-r = float(input('Enter the yearly rate of interest(r): '))
+r = input('Enter the yearly rate of interest(r): ')
 
 #Prompt the user to input the number of years until maturation
-t = int(input('Enter the number of years until maturation(t): '))
+t = input('Enter the number of years until maturation(t): ')
 
 #Prompt the user to input the number of times the interest is compounded per year
-n = int(input('Enter the number of times the interest is compounded per year(n): '))
+n = input('Enter the number of times the interest is compounded per year(n): ')
 
 def investment(C, r, n, t):
-    p = round(C * (((1 + (r/n)) ** (t*n))), 2)
-    print('The final value of the investment to the nearest penny is: ', p)
-    return p
+    try:
+        C = float(C)
+        r = float(r)
+        t = float(t)
+        n = float(n)
+        p = round(C * (((1 + (r/n)) ** (t*n))), 2)
+        print('The final value of the investment to the nearest penny is: ', p)
+        return p
+    except:
+        print('Please, Enter a float value')
 investment(C, r, n, t)
--- src/chapter4/exercise6.py
+++ src/chapter4/exercise6.py
@@ -1,13 +1,18 @@
 #Prompt the user for hours
-hours = float(input("Enter Hours: "))
+hours = input("Enter Hours: ")
 #Prompt the user for the rate
-rate = float(input("Enter Rate: "))
+rate = input("Enter Rate: ")
 
 def computepay(hours, rate):
-    if hours <= 40:
-        wage = hours * rate
-    else:
-        wage = 40 * rate + (hours - 40) * 1.5 * rate
-    return wage
+    try:
+        hours = float(hours)
+        rate = float(rate)
+        if hours <= 40:
+            wage = hours * rate
+        else:
+            wage = 40 * rate + (hours - 40) * 1.5 * rate
+        print('Pay: ', wage)
+        return wage
+    except:
+        print('Please, enter a float value')
 pay = computepay(hours, rate)
-print('Pay: ', pay)
