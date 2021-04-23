08d24a8bd094bd2d5d42992b4b07e24389e2174a
group9BCS1 <allanpaulsoc@gmail.com>
2021-04-23 13:29:59 +0300

    chapter5

--- src/chapter5/exercise1.py
+++ src/chapter5/exercise1.py
@@ -1,16 +1,17 @@
-sum = 0
+total = 0
 count = 0
-average = 0
-while True:
-  try:
-    x = input("Enter a number: ")
-    if x == "done":
-     break
-    value = int(x)
-    sum = value + sum
-    count = count + 1
-    average = sum / count
-  except:
-    print("Invalid input")
-print(sum, count, average)
+avg = 0
 
+while True:
+    x = input('Enter a number: ')
+    try:
+        x = int(x)
+        total = total + x
+        count = count + 1
+        avg = total/count      
+    except:
+        if x == 'done':                     #when user types done, total, number of values entered and average is printed
+            print(total, count, avg)
+            break
+        else:
+            print('Invalid input')
--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,101 +1,99 @@
-#This is exercise3 for Group9BCS
-"""Group 9 members
-1. WESONGA BOB 2020/BCS/092/PS
-2. ARYAZAYO KEITH 2020/BCS/022/PS
-3. MAYANJA ROBERT 2020/BCS/043/PS
-4. OGENA ALLAN PAUL 2020/BCS/003
-"""
-#22nd April,2021
-#This is a program that will stimulate a simple change maker for a vending machine
-
-# creating function to accept deposit
-def deposit():
-    purchase_price(price1)
-    total_deposit = 0
-    while price1 > total_deposit:
-        test_price = price1
-        dep = input("Indicate your deposit:")
-        if dep == "c":
-            print("Take the change below")  # Function change
-            break
-        elif dep == "f":
-            test_price = test_price - 5
-            purchase_price(test_price)
-            total_deposit = total_deposit + 5
-            continue
-        elif dep == "o":
-            test_price = test_price - 1
-            purchase_price(test_price)
-            total_deposit = total_deposit + 1
-            continue
-        elif dep == "q":
-            test_price = test_price - 0.25
-            purchase_price(test_price)
-            total_deposit = total_deposit + 0.25
-            continue
-        elif dep == "d":
-            test_price = test_price - 0.1
-            purchase_price(test_price)
-            total_deposit = total_deposit + 0.1
-            continue
-        elif dep == "n":
-            test_price = test_price - 0.05
-            purchase_price(test_price)
-            total_deposit = total_deposit + 0.05
-            continue
-        else:
-            print("Illegal selection:", dep)
-            continue
-    # print(total_deposit)
-
-
-# creating function that prints purchase price
-def purchase_price(price):
-    a = price // 1  # Gives number of dollars
-    b = price % 1  # Gives number of cents
-    if a == 0:
-        payment_due = print(f"Payment due:{b * 100:.0f} cents")
-    else:
-        payment_due = print(f"Payment due:{a:.0f} dollars and {b * 100:.0f} cents")
-
-
 # creating function for menu of deposit selection
 def deposit_menu():
-    print("\nMenu for deposits:\n")
-    print("""  'n' - deposit a nickel
-  'd' - deposit a dime
-  'q' - deposit a quarter
-  'o' - deposit a one dollar bill
-  'f' - deposit a five dollar bill
-  'c' - cancel the purchase
+    print("""
+Menu for deposits:
+'n' - deposit a nickel
+'d' - deposit a dime
+'q' - deposit a quarter
+'o' - deposit a one dollar bill
+'f' - deposit a five dollar bill
+'c' - cancel the purchase
+    """)
+
+def paymentDue(price):
+    a = price // 1      # Gives number of dollars
+    b = price % 1       # Gives number of cents
+    print(f"Payment due: {a:.0f} dollars and {b*100:.0f} cents")
+
+
+#main program starts here
+n = 25
+d = 25
+q = 25
+o = 0
+f = 0
+
+print(f"""Welcome to the vending machine change maker program
+Change maker initialized.
+Stock contains:
+   {n} nickles
+   {d} dimes
+   {q} quaters
+   {o} ones
+   {f} fives
 """)
 
-
-# main program
-# initializing money
-nickels = 25
-dimes = 25
-quarters = 25
-ones = 0
-fives = 0
-print("Welcome to the vending machine change maker program")
-print("Change maker initialized.")
-print(f"Stock contains:\n {nickels} nickels\n {dimes} dimes\n {quarters} quarters\n {ones} ones\n {fives} fives")
 while True:
     try:
-        # requesting for price input
-        price = input("Enter the purchase price (xx.xx) or `q' to quit:")
+        #requesting price
+        price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
         if price == "q":
-            print("Quiting....")
+            print("Quiting...")
             break
-        price1 = float(price)
-        if (price1 > 0) and ((price1 * 100) % 5 == 0):
+        price = float(price)
+        priceDue = price
+        if (price > 0) and (((price*100) % 5) == 0):
             deposit_menu()
-            deposit()
+            paymentDue(price)
+            total_deposit = 0.00
+            while price > total_deposit:
+                dep = input("Indicate your deposit: ")
+                if dep == 'c':
+                    print("Please take the change below",total_deposit)  # Function change
+
+                    break
+                elif dep == 'n':
+                    priceDue = priceDue - 0.05
+                    total_deposit = total_deposit + 0.05
+                    n = n + 1
+                    paymentDue(priceDue)
+                    continue
+                elif dep == 'd':
+                    priceDue = priceDue - 0.1
+                    total_deposit = total_deposit + 0.1
+                    d = d + 1
+                    paymentDue(priceDue)
+                    continue
+                elif dep == 'q':
+                    priceDue = priceDue - 0.25
+                    total_deposit = total_deposit + 0.25
+                    q = q + 1
+                    paymentDue(priceDue)
+                    continue
+                elif dep == 'o':
+                    priceDue = priceDue - 1.0
+                    total_deposit = total_deposit + 1
+                    o = o + 1
+                    paymentDue(priceDue)
+                    continue
+                elif dep == 'f':
+                    priceDue = priceDue - 5.0
+                    total_deposit = total_deposit + 5
+                    f = f + 1
+                    paymentDue(priceDue)
+                    continue
+                else:
+                    print("Illegal selection:", dep)
+                    paymentDue(price)
+                    continue
+            if total_deposit == price:
+                print("No chnge.")
+                print(f"Stock contains:\n {n} nickles\n {d} dimes\n {q} quaters\n {o} ones\n {f} fives ")
+            else:
+                print("Take your change:\n ")
+                print(f"Stock contains: \n {n} nickles\n {d} dimes\n {q} quaters\n {o} ones\n {f} fives ")   
         else:
             print("Illegal price: Must be a non-negative multiple of 5 cents.")
     except:
-        print("Invalid input!!")
+        print("Invalid input!")
         continue
-
-
