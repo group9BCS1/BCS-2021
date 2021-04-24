9a3170b3c98048dde77fae0ac586362a82f00061
group9BCS1 <allanpaulsoc@gmail.com>
2021-04-24 14:42:20 +0300

    exercise3

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,19 +1,16 @@
 # creating function for menu of deposit selection
 def deposit_menu():
-    print("""
-Menu for deposits:
-'n' - deposit a nickel
-'d' - deposit a dime
-'q' - deposit a quarter
-'o' - deposit a one dollar bill
-'f' - deposit a five dollar bill
-'c' - cancel the purchase
-    """)
+    print("Menu for deposits:\n  'n' - deposit a nickel\n  'd' - deposit a dime\n  'q' - deposit a quarter\n  'o' - deposit a one dollar bill\n  'f' - deposit a five dollar bill\n  'c' - cancel the purchase\n")
 
 def paymentDue(price):
     a = price // 1      # Gives number of dollars
     b = price % 1       # Gives number of cents
-    print(f"Payment due: {a:.0f} dollars and {b*100:.0f} cents")
+    if total_deposit_due < price and a != 0:
+        print(f"Payment due: {a:.0f} dollars and {b*100:.0f} cents")
+    elif total_deposit_due < price and a == 0 and b != 0:
+        print(f"Payment due: {b*100:.0f} cents")
+
+
 
 
 #main program starts here
@@ -33,10 +30,12 @@ Stock contains:
    {f} fives
 """)
 
+
 while True:
+    #requesting price
+    price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
     try:
-        #requesting price
-        price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
+        
         if price == "q":
             print("Quiting...")
             break
@@ -44,14 +43,53 @@ while True:
         priceDue = price
         if (price > 0) and (((price*100) % 5) == 0):
             deposit_menu()
-            paymentDue(price)
             total_deposit = 0.00
+            total_deposit_due = total_deposit
+            paymentDue(price)
             while price > total_deposit:
                 dep = input("Indicate your deposit: ")
                 if dep == 'c':
-                    print("Please take the change below",total_deposit)  # Function change
+                    if total_deposit == 0 :     #no refund given when amount deposited is 0
+                        print("No refund.")
+                        break
+                    elif total_deposit > 0 :
+                        print("Please take your refund:\n")  # Function change
+                        r_quarter = int(total_deposit//0.25)
+                        total_deposit = total_deposit % 0.25
+                        r_dime = int(total_deposit // 0.1)
+                        total_deposit = total_deposit % 0.1
+                        r_nickle = int(total_deposit // 0.05)
 
-                    break
+
+                        if (r_quarter == 0) and (r_dime ==0) and (r_nickle !=0):
+                            print(f"{r_nickle} nickles")
+                            n = n - r_nickle
+                        elif (r_quarter == 0) and (r_dime !=0) and (r_nickle ==0):
+                            print(f"{r_dime} dimes")
+                            d = d - r_dime
+                        elif (r_quarter != 0) and (r_dime ==0) and (r_nickle ==0):
+                            print(f"{r_quarter} quaters")
+                            q = q - r_quarter
+                        elif (r_quarter == 0) and (r_dime !=0) and (r_nickle !=0):
+                            print(f"{r_dime} dimes\n{r_nickle} nickles")
+                            d = d - r_dime
+                            n = n - r_nickle
+                        elif (r_quarter != 0) and (r_dime ==0) and (r_nickle !=0):
+                            print(f"{r_quarter} quaters\n{r_nickle} nickles")
+                            q = q - r_quarter
+                            n = n - r_nickle
+                        elif (r_quarter != 0) and (r_dime !=0) and (r_nickle ==0):
+                            print(f"{r_quarter} quaters\n{r_dime} dimes")
+                            q = q - r_quarter
+                            d = d - r_dime
+                        elif (r_quarter != 0) and (r_dime !=0) and (r_nickle !=0):
+                            print(f"{r_quarter} quaters\n{r_dime} dimes\n{r_nickle} nickles")
+                            q = q - r_quarter
+                            d = d - r_dime
+                            n = n - r_nickle
+                            
+
+                        break
                 elif dep == 'n':
                     priceDue = priceDue - 0.05
                     total_deposit = total_deposit + 0.05
@@ -86,12 +124,53 @@ while True:
                     print("Illegal selection:", dep)
                     paymentDue(price)
                     continue
-            if total_deposit == price:
-                print("No chnge.")
+            if dep == "c":   # if the user had cancelled payment, this makes the program just pass back to the beginning
                 print(f"Stock contains:\n {n} nickles\n {d} dimes\n {q} quaters\n {o} ones\n {f} fives ")
-            else:
-                print("Take your change:\n ")
-                print(f"Stock contains: \n {n} nickles\n {d} dimes\n {q} quaters\n {o} ones\n {f} fives ")   
+                pass
+            elif total_deposit == price:
+                print("No change.")
+                print(f"Stock contains:\n {n} nickles\n {d} dimes\n {q} quaters\n {o} ones\n {f} fives ")
+                continue
+            elif total_deposit > price and total_deposit != price:
+                print("Take your change: ")
+                change = round(total_deposit - price,2)     #Change to be dispensed
+
+                c_quarter = int(change // 0.25)
+                change = change % 0.25
+                c_dime = int(change//0.1)
+                change = change % 0.1
+                c_nickle = int(change//0.05)
+                if (c_quarter == 0) and (c_dime ==0) and (c_nickle !=0):
+                    print(f"{c_nickle} nickles")
+                    n = n - c_nickle
+                elif (c_quarter == 0) and (c_dime !=0) and (c_nickle ==0):
+                    print(f"{c_dime} dimes")
+                    d = d - c_dime
+                elif (c_quarter != 0) and (c_dime ==0) and (c_nickle ==0):
+                    print(f"{c_quarter} quaters")
+                    q = q - c_quarter
+                elif (c_quarter == 0) and (c_dime !=0) and (c_nickle !=0):
+                    print(f"{c_dime} dimes\n{c_nickle} nickles")
+                    d = d - c_dime
+                    n = n - c_nickle
+                elif (c_quarter != 0) and (c_dime ==0) and (c_nickle !=0):
+                    print(f"{c_quarter} quaters\n{c_nickle} nickles")
+                    q = q - c_quarter
+                    n = n - c_nickle
+                elif (c_quarter != 0) and (c_dime !=0) and (c_nickle ==0):
+                    print(f"{c_quarter} quaters\n{c_dime} dimes")
+                    q = q - c_quarter
+                    d = d - c_dime
+                elif (c_quarter != 0) and (c_dime !=0) and (c_nickle !=0):
+                    print(f"{c_quarter} quaters\n{c_dime} dimes\n{c_nickle} nickles")
+                    q = q - c_quarter
+                    d = d - c_dime
+                    n = n - c_nickle
+
+
+
+                print(f"Stock contains: \n {n} nickles\n {d} dimes\n {q} quaters\n {o} ones\n {f} fives ") 
+                continue  
         else:
             print("Illegal price: Must be a non-negative multiple of 5 cents.")
     except:
