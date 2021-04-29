c7da06ac5855fd859eb65ce8dfec4dc8a7671efa
group9BCS1 <allanpaulsoc@gmail.com>
2021-04-29 12:11:51 +0300

    simple version of program

---
+++ src/mid_term/project.py
@@ -0,0 +1,10 @@
+while True:
+    code = input("Enter code: ")
+    if code == "r" or code == "R":
+        print(code)
+    elif code == "c" or code == "C":
+        print(code)
+    elif code == "i" or code == "I":
+        print(code)
+    else:
+        print("Enter one of the following:\n  r - for residendital\n  c - for commercial\n  i - for industrial")
