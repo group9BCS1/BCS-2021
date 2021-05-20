644d9be18d196d70d517d29c542dde8d3ccfe374
group9BCS1 <allanpaulsoc@gmail.com>
2021-05-20 19:22:03 +0300

    project

--- src/project_02/project2_a.py
+++ src/project_02/project2_a.py
@@ -10,17 +10,24 @@ if len(year) > 4:        #this ensures entry has a maximum of four
     exit("Invalid entry of year!")
 
 try:                         #This is for checking if entry of year is an interger or 'all/ALL'
-    if year == "all" or year == "ALL" or (year == int(year)):
+    if year == "" or year == "all" or year == "ALL" or (year == int(year)):
         pass
 except:
     print("Invalid entry of year!")
     exit()
 
-new_file = open(file_name,'w')      #incase the name file exists, this will overwrite it
+new_file = open(file_name,'w+')      #incase the name file exists, this will overwrite it
+count = 0
 for line in measles:
     if (line[88:92] == year) or (line[88:91] == year) or (line[88:90] == year) or (line[88:89] == year):
-        new_file.write(line)    #add content to the file
-    elif year == line[88:88] or year == "all" or year == "ALL":
-        new_file.write(line)       #add content to the file
+        new_file.write(line)    #add content to the new file
+        count += 1
+    elif year == "" or year == "all" or year == "ALL":
+        new_file.write(line)       #add content to the new file
+        count += 1
 
-print("Process complete Check your created file.")
+if count > 0:
+    print("Process complete Check your created file.")
+else:
+    print("Year doesn't exist in records")
+    exit()
